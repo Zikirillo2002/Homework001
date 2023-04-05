1b65f9365b409fe1558034d462950e5c8f354b89
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-05 07:02:40 +0500

    Added Task number 7

--- homework001/Program.cs
+++ homework001/Program.cs
@@ -1,7 +1,46 @@
-﻿internal class Program
+﻿using System.ComponentModel;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+            // Task condition: Write a program to calculate the factorial of a given number.
+            //                 Sample input: 5
+            //                 Result: “Factorial of 5 is: 120.” 
+
+        int number;
+        long result = 1;
+        bool isnumber;
+
+        Console.Write("Enter the number : ");
+            isnumber = int.TryParse(Console.ReadLine(), out number);
+                Console.WriteLine();
+
+        while (!isnumber || number < 0)
+        {
+            Console.WriteLine($"An error occurred in the system !!! \n Please enter another number !!! ");
+                Console.WriteLine();
+                    Console.Write("Enter the number : ");
+                        isnumber = int.TryParse(Console.ReadLine(), out number);
+                            Console.WriteLine();
+        }
+
+        if (number == 0)
+        {
+            Console.WriteLine("The factorial of 0 is : 1");
+        }
+
+        else
+        {
+            for (int i = number; i >= 1; i--)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"The factorial of {number} is : {result}");
+        }
+
+        Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine();
+                Main(args); 
     }
 }
