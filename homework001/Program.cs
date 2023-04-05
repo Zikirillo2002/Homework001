d18fc44b6813a63465087a3c136a8095324d3878
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-04-05 05:42:42 +0500

    Added Task number 4

--- homework001/Program.cs
+++ homework001/Program.cs
@@ -2,6 +2,39 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Task condition : Write a program which accepts a number from user and prints all integers in range [1, 100] 
+        //                  divisible by it.Prepare solution using: 
+        //                  • For
+        //                  • While
+
+        int number, j = 0;
+        bool isnumber;
+
+        Console.Write("Enter  the number : ");
+            isnumber = int.TryParse(Console.ReadLine(), out number);
+                Console.WriteLine();
+
+        while (!isnumber || number <= 0)
+        {
+            Console.WriteLine($"An error occurred in the system !!! \n Please enter another number  !!! ");
+                Console.WriteLine();
+                    Console.Write("Enter the number : ");
+                        isnumber = int.TryParse(Console.ReadLine(), out number);
+                            Console.WriteLine();
+        }
+
+        for (int i = 1; i <= 100; i++)
+        {
+            if (number % i == 0)
+            {
+                j++;
+                Console.WriteLine($" {j}) --> {i}");
+            }
+        }
+
+        Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine();
+                Main(args);
+
     }
 }
